3b555830787ed285e5fb8891b36797b008e178a3
JakeOldham <jakemoldham@gmail.com>
2018-01-08 20:54:25 -0700

    updated resume and upgraded look and feel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,10 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def download_resume
+    send_file "#{Rails.root}/app/assets/images/kadenresume.pdf", type: "application/pdf", x_sendfile: true
+  end
+
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get 'download_resume', to: "static_pages#download_resume"
   get '/about', to: 'static_pages#about', as: :about
   resources :portfolios
   get '/users/sign_up', to: redirect('/users/sign_in')
